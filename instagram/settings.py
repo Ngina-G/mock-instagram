e10f989a7ffd8421581a87d34921f4c421b3eb1b
Ngina <Nginagitau11@outlook.com>
2022-06-07 18:35:13 +0300

    update; adds bootstrap 5

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clone.apps.CloneConfig'
+    # installed
+    'clone.apps.CloneConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
