b085e099d8c330a9d200c4d1e7d85149c527f080
Ngina <Nginagitau11@outlook.com>
2022-06-07 19:52:49 +0300

    update: adds post and profile

--- clone/admin.py
+++ clone/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Image, Comment
+from .models import Post, Comment, Profile
 # Register your models here.
 
 admin.site.register(Comment)
-admin.site.register(Image)
+admin.site.register(Post)
+admin.site.register(Profile)
+
