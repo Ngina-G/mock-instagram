0f8d3d92515829180353569b8a3b89ac986b08ec
Ngina <Nginagitau11@outlook.com>
2022-06-07 19:05:33 +0300

    update: adds login configurations

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('clone.urls'))
+    path('',include('clone.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('logout/', views.logout, {"next_page": '/'})
 ]
