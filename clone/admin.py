d0f84bd1e645d3fe6d8749411a0078ca9d0af267
Ngina <Nginagitau11@outlook.com>
2022-06-08 08:24:37 +0300

    deployment to heroku

--- clone/admin.py
+++ clone/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Profile, User
+from .models import Post, Comment, Profile
 # Register your models here.
 
 admin.site.register(Comment)
 admin.site.register(Post)
 admin.site.register(Profile)
-admin.site.register(User)
+# admin.site.register(User)
